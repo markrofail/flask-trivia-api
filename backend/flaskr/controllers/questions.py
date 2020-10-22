ac153b34ae49d4f5db94a8b0d65c3e4fb5b5ecb2
Mark Rofail <mark.rofail@shahry.app>
2020-10-22 10:54:30 +0200

    [feat] implement POST /questions/search

--- backend/flaskr/controllers/questions.py
+++ backend/flaskr/controllers/questions.py
@@ -1,10 +1,10 @@
-from flask import jsonify, request, Blueprint
-
-from models import Question, Category
+from flask import Blueprint, abort, jsonify, request
 from flaskr.services.categories import get_all_categories, get_category
-from flaskr.services.questions import get_all_questions, get_question
+from flaskr.services.questions import (get_all_questions, get_question,
+                                       search_question_by_text)
+from models import Category, Question
 
-questions_api = Blueprint('questions', '')
+questions_api = Blueprint("questions", "")
 
 
 @questions_api.route("/categories/<category_id>/questions/")
@@ -12,7 +12,7 @@ def get_question_by_category(category_id):
     current_category = get_category(category_id, return_json=True)
     categories = get_all_categories(return_json=True)
 
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     questions = get_all_questions(category_id, page, return_json=True)
 
     return jsonify(
@@ -20,20 +20,32 @@ def get_question_by_category(category_id):
             current_category=current_category,
             questions=questions,
             question_count=len(questions),
-            categories=categories
+            categories=categories,
         )
     )
 
 
-@questions_api.route("/questions/<question_id>", methods=['GET'])
+@questions_api.route("/questions/<question_id>", methods=["GET"])
 def get_question_detail(question_id):
     question = get_question(question_id, return_json=True)
     return jsonify(dict(question=question, success=True))
 
 
-@questions_api.route("/questions/<question_id>", methods=['DELETE'])
+@questions_api.route("/questions/<question_id>", methods=["DELETE"])
 def delete_question_detail(question_id):
     question = get_question(question_id)
     question.delete()
 
     return jsonify(dict(question_id=question_id, success=True))
+
+
+@questions_api.route("/questions/search", methods=["POST"])
+def search_question():
+    search_query = request.json.get("query", None)
+    if not search_query:
+        abort(422)
+
+    questions = search_question_by_text(search_query, return_json=True)
+    return jsonify(
+        dict(questions=questions, question_count=len(questions), success=True)
+    )
--- backend/flaskr/services/questions.py
+++ backend/flaskr/services/questions.py
@@ -2,21 +2,32 @@ from models import Question
 
 QUESTIONS_PER_PAGE = 10
 
+
 def get_all_questions(category_id, page=1, return_json=False):
     questions = (
-        Question.query
-        .filter_by(category=category_id)
+        Question.query.filter_by(category=category_id)
         .paginate(page, QUESTIONS_PER_PAGE, False)
         .items
     )
-    
+
     if return_json:
-        questions=[q.format() for q in questions]
+        questions = [q.format() for q in questions]
     return questions
+
 
 def get_question(question_id, return_json=False):
     question = Question.query.get_or_404(question_id)
 
     if return_json:
-        question=question.format(detail=True)
+        question = question.format(detail=True)
     return question
+
+
+def search_question_by_text(search_query, return_json=False):
+    questions = Question.query.filter(
+        Question.question.ilike(f"%{search_query}%")
+    ).all()
+
+    if return_json:
+        questions = [q.format() for q in questions]
+    return questions
