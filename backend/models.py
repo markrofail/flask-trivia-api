62f5c69ba28dea818f85d399ac965cdb1a55d3d2
Mark Rofail <mark.rofail@shahry.app>
2020-10-23 16:15:42 +0200

    [fix] make question <> category relationship

--- backend/models.py
+++ backend/models.py
@@ -1,84 +1,85 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
-from flask_sqlalchemy import SQLAlchemy
 import json
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import relationship
 
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = "postgres://{}@{}/{}".format(
+    "postgres", "localhost:5432", database_name
+)
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
+    """
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    """
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-'''
-Question
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
+class Category(db.Model):
+    __tablename__ = "categories"
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    questions = relationship("Question", back_populates="category")
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def __init__(self, type):
+        self.type = type
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def format(self):
+        return {"id": self.id, "type": self.type}
 
-  def format(self, details=False):
-    payload = {
-        'id': self.id,
-        'question': self.question,
-    }
 
-    if details:
-      payload.update({
-          'answer': self.answer,
-          'category': self.category,
-          'difficulty': self.difficulty
+class Question(db.Model):
+    __tablename__ = "questions"
 
-      })
-    return payload
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    difficulty = Column(Integer)
 
-'''
-Category
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship(Category, back_populates="questions")
 
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def __init__(self, type):
-    self.type = type
+    def update(self):
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self, details=False):
+        payload = {
+            "id": self.id,
+            "question": self.question,
+        }
+
+        if details:
+            payload.update(
+                {
+                    "answer": self.answer,
+                    "category": self.category,
+                    "difficulty": self.difficulty,
+                }
+            )
+        return payload
