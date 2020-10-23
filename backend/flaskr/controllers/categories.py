da70230febe6893a8c8066d24d5be1addd5b600d
Mark Rofail <mark.rofail@shahry.app>
2020-10-23 12:21:32 +0200

    [fix] switch categories api to function based

--- backend/flaskr/controllers/categories.py
+++ backend/flaskr/controllers/categories.py
@@ -1,13 +1,10 @@
-from flask import jsonify, views, Blueprint
-
+from flask import Blueprint, jsonify, views
 from flaskr.services.categories import get_all_categories
 
-categories_api = Blueprint('categories', '')
+categories_api = Blueprint("categories", "")
 
-class CategoryAPI(views.MethodView):
 
-    def get(self):
-        categories = get_all_categories(return_json=True)
-        return jsonify(categories)
-
-categories_api.add_url_rule('/categories', view_func=CategoryAPI.as_view('categories'))
+@categories_api.route('/categories')
+def list_categories(self):
+    categories = get_all_categories(return_json=True)
+    return jsonify(categories)
